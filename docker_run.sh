3d72203dda7a01aa464f28bc9d8615e6db9f14a6
ApexioDaCoder <apexiodacoder@gmail.com>
2022-01-01 21:55:09 -0600

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -13,7 +13,7 @@ docker run -d \
     -e TZ="America/Chicago" \
     -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
-    --dns=127.0.0.1 --dns=1.1.1.1 \
+    --dns=1.1.1.1 \
     --restart=unless-stopped \
     --hostname pi.hole \
     -e VIRTUAL_HOST="pi.hole" \
